0f40a5f64c4ee7aeea14b2249741b8d3a48c6589
Hemal Gadhiya <gadhiy@amazon.com>
2022-12-22 14:27:53 -0500

    test

--- src/main/java/com/amazonaws/kafka/samples/RunConsumer.java
+++ src/main/java/com/amazonaws/kafka/samples/RunConsumer.java
@@ -94,7 +94,7 @@ class RunConsumer implements Callable<String> {
                     propagationlist.add(System.currentTimeMillis() - record.value().getEventtimestamp());
                     logger.info(System.currentTimeMillis() - record.value().getEventtimestamp());
                     if (numberOfMessages % 1000 == 0) {
-                        perc = (double) Quantiles.percentiles().index(99).compute(propagationlist);
+                        perc = Quantiles.percentiles().index(99).compute(propagationlist);
                         avgPropagationDelay = (double) propagationDelay / numberOfMessages;
                         logger.info("{} - P99 in millisecond: {} \n", Thread.currentThread().getName(), perc);
                         logger.info("{} - Avg Propagation delay in milliseconds: {} \n",
